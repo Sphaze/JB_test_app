69b995f056829d9c343b5e68fb0ae74dce21d8bc
Conor McN <conorm967@gmail.com>
2022-07-03 16:27:17 +0100

    made modifications

--- data_recorder/admin.py
+++ data_recorder/admin.py
@@ -15,22 +15,32 @@ class PostAdmin(admin.ModelAdmin):
 
     exclude = ('est_completion_time', 'downtime') # hide these fields by excluding them
     readonly_fields = ('est_completion_time_readonly', 'downtime_readonly') # display these new fields as readonly
-    
-    html_title = "d = days&nbsp;&nbsp;&nbsp;&nbsp;h = hours&nbsp;&nbsp;&nbsp;&nbsp;m = minutes<br></br>"
+
+   # html_title = "d = days&nbsp;&nbsp;&nbsp;&nbsp;h = hours&nbsp;&nbsp;&nbsp;&nbsp;m = minutes<br></br>"
 
     @admin.display(description="Duration of estimated completion time")
     def est_completion_time_readonly(self, obj):
             
         str_output = str(obj.est_completion_time)
         str_output_list = str_output.split(':')
-   
-        if int(str_output_list[0]) == 0: #if hours is zero, only print minutes
 
-            formatted_str = str_output_list[1] + "m"
+        modify_hour_string = str_output_list[0]
+        hour_num = modify_hour_string[0] #this is like charAt[0] in Java, I capture the hour number
+
+        if  hour_num == "1": #if hours is one, print "hour"
+            str_choice_hr = " hour "
         else:
-            formatted_str = str_output_list[0] + "h " + str_output_list[1] + "m"  # any days will be automatically shown because of the ISO time format
+            str_choice_hr = " hours "
+
+        if  str_output_list[1] == "01": #if minutes is one, print "minute"
+            str_choice_min = " minute"
+        else:
+            str_choice_min = " minutes"
+
+
+        formatted_str = str_output_list[0] + str_choice_hr + str_output_list[1] + str_choice_min  # any days will be automatically shown because of the ISO time format
       
-        return format_html(self.html_title + '<textarea id="1" cols="40" rows="1" readonly>{}</textarea>', formatted_str)
+        return format_html('<textarea id="1" cols="40" rows="1" readonly>{}</textarea>', formatted_str)
 
     @admin.display(description="Duration of downtime")
     def downtime_readonly(self, obj):
@@ -38,12 +48,22 @@ class PostAdmin(admin.ModelAdmin):
         str_output = str(obj.downtime)
         str_output_list = str_output.split(':')
 
-        if int(str_output_list[0]) == 0: #if hours is zero, only print minutes
-            formatted_str2 = str_output_list[1] + "m"
+        modify_hour_string = str_output_list[0]
+        hour_num = modify_hour_string[0] #this is like charAt[0] in Java, I capture the hour number
+
+        if  hour_num == "1": #if hours is one, print "hour"
+            str_choice_hr = " hour "
         else:
-            formatted_str2 = str_output_list[0] + "h " + str_output_list[1] + "m"  # any days will be automatically shown because of the ISO time format
-        
-        return format_html(self.html_title + '<textarea id="1" cols="40" rows="1" readonly>{}</textarea>', formatted_str2)
+            str_choice_hr = " hours "
+
+        if  str_output_list[1] == "01": #if minutes is one, print "minute"
+            str_choice_min = " minute"
+        else:
+            str_choice_min = " minutes"
+
+        formatted_str = str_output_list[0] + str_choice_hr + str_output_list[1] + str_choice_min  # any days will be automatically shown because of the ISO time format
+      
+        return format_html('<textarea id="1" cols="40" rows="1" readonly>{}</textarea>', formatted_str)
 
     # unused code
     #spare = []
